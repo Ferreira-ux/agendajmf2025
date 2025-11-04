b1bb6f2f97e1276577cde64b1783673cf0a32136
Suellen Ferreira <suellenveia@gmail.com>
2025-11-04 08:14:36 -0300

    Adicionei todos os arquivos da Agenda JMF 2025

---
+++ paginas/conteudo/del-contato.php
@@ -0,0 +1,41 @@
+<?php
+include('../../config/conexao.php');
+
+if(isset($_GET['idDel'])){
+    $id = $_GET['idDel'];
+
+    $select = "SELECT foto_contatos FROM tb_contatos WHERE id_contatos=:id";
+    try {
+        $result = $conect->prepare($select);
+        $result->bindValue(':id', $id, PDO::PARAM_INT);
+        $result->execute();
+
+        $contar = $result->rowCount();
+        if ($foto != 'avatar-padrao.png') {
+            $filePath = "../../img/cont/" . $foto;
+
+            if (file_exists($filePath)) {
+                unlink($filePath);
+            }
+        }
+        $delete = "DELETE FROM tb_contatos WHERE id_contatos = :id";
+        try {
+            $result = $conect->prepare($delete);
+            $result->bindValue(':id', $id, PDO::PARAM_INT);
+            $result->execute();
+
+            $contar = $result->rowCount();
+            if ($contar > 0) {
+                header("Location: ../home.php");
+            } else {
+                header("Location: ../home.php");
+            }
+
+        } catch (PDOException $e) {
+            echo "<strong>ERRO DE DELETE: </strong>" . $e->getMessage();
+        }
+    } catch (PDOException $e) {
+        echo "<strong>ERRO DE DELETE: </strong>" . $e->getMessage();
+
+    }
+} else {header("Location: ../home.php");}
